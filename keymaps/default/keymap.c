4b945b3aa25f95f60eec4e542808da789e953cdc
sidharta <jeansidharta@gmail.com>
2024-12-17 19:09:57 -0300

    Moved quote position

--- keymaps/default/keymap.c
+++ keymaps/default/keymap.c
@@ -59,12 +59,12 @@ const uint16_t PROGMEM keymaps[][MATRIX_ROWS][MATRIX_COLS] = {
         // left
         KC_MINS, KC_7, KC_8,    KC_9,    KC_0,
         KC_EQL,  KC_4, KC_5,    KC_6,    KC_BSLS,
-        KC_SLSH, KC_1, KC_2,    KC_3,    KC_GRV,
+        KC_QUOT, KC_1, KC_2,    KC_3,    KC_GRV,
                        KC_TRNS, KC_TRNS, MO(QWERTY_4),
 
         // right
         KC_WH_U, XXXXXXX, KC_MS_U, KC_RBRC, KC_LBRC,
-        KC_WH_D, KC_MS_R, KC_MS_D, KC_MS_L, KC_QUOT,
+        KC_WH_D, KC_MS_R, KC_MS_D, KC_MS_L, XXXXXXX,
         KC_BTN5, KC_BTN4, KC_BTN3, KC_BTN2, KC_BTN1,
         _______, _______, _______
     ),
